ee075f49c33434cb8730ea5b6fc5f4c51fc42d6f
Marshal Tudu <iammarshal22@gmail.com>
2025-04-04 21:12:29 +0530

    fix: Disable image optimization to resolve Cloud Run 400 errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    // Disable Next.js Image Optimization (as it resolves Cloud Run 400 errors)
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
