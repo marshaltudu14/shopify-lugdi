9e6b9ca1b5c9a8ed61f3f5ff175c0a5b1b7484f4
Marshal Tudu <iammarshal22@gmail.com>
2025-04-17 00:23:05 +0530

    Updated sizeChart

--- lib/sizeChartProductTypeMap.ts
+++ lib/sizeChartProductTypeMap.ts
@@ -1,5 +1,5 @@
 export const sizeChartProductTypeMap: Record<string, string[]> = {
-  "Unisex Oversized T-Shirt": ["Men Oversized T-Shirt",],
+  "Unisex Oversized T-Shirt": ["Men Oversized T-Shirt", "Women Oversized T-Shirt"],
   "Unisex Classic T-Shirt": ["Men Classic T-Shirt", "Women Classic T-Shirt"],
   "Crop Top": ["Women Crop Top"],
   // Add more mappings as needed
